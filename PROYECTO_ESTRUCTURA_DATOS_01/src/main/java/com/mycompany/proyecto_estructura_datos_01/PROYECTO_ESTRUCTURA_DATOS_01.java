6fceffa69a2d444a8772005a8cee3ea4950fd24b
evildead191 <parias40359@ufide.ac.cr>
2025-03-01 15:30:17 -0600

    Se editó la clase ListaEnlazadaSimple y la clase Usuario

--- PROYECTO_ESTRUCTURA_DATOS_01/src/main/java/com/mycompany/proyecto_estructura_datos_01/PROYECTO_ESTRUCTURA_DATOS_01.java
+++ PROYECTO_ESTRUCTURA_DATOS_01/src/main/java/com/mycompany/proyecto_estructura_datos_01/PROYECTO_ESTRUCTURA_DATOS_01.java
@@ -3,8 +3,6 @@ package com.mycompany.proyecto_estructura_datos_01;
 public class PROYECTO_ESTRUCTURA_DATOS_01 {
 
     public static void main(String[] args) {
-        
-        
-        
+        Menu.main(args);
     }
 }
